d87648f0756065e8faa9efe4cfb1d19f92e42d89
Marcel Hernandez <e6990620@gmail.com>
2015-09-13 16:33:19 +0200

    remote_backup.sh is now forced to use an *encrypted* SSH private key

--- scripts/remote_backup.sh
+++ scripts/remote_backup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Dependencies: duplicity, gpg, pycrypto (python-crypto on Ubuntu), pip, paramiko (pip package)
-PASSPHRASE= SIGN_PASSPHRASE=$(cat [/path/to/gpg_passphrase]) /usr/bin/duplicity \
+## Script dependencies
+# duplicity
+# OpenSSH client
+# gnupg
+# pycrypto (python-crypto package on Ubuntu)
+# pip
+# paramiko (pip package)
+
+GPGPASS=$(cat [/path/to/gpg_passphrase])
+SSHPASS=$(cat [/path/to/ssh_passphrase])
+
+FTP_PASSWORD=$SSHPASS PASSPHRASE= SIGN_PASSPHRASE=$GPGPASS /usr/bin/duplicity \
     --volsize=500 \
     --encrypt-sign-key [SHORT GPG KEYID] \
     --ssh-options="-oIdentityFile=[/path/to/ssh_private_key]" \
